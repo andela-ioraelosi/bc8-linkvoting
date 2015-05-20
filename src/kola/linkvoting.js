b0b2d25ef652daf067c19b694acebaa08278b516
Kolawole ERINOSO <superprofessor23@gmail.com>
2015-05-20 15:38:35 +0100

    "Edits the edit page and the associated js file"

--- src/kola/linkvoting.js
+++ src/kola/linkvoting.js
@@ -2,35 +2,85 @@
 
 var Category = Backbone.Model.extend({
 	defaults: {
-		hashtagCategory: 'Others',
-		title: '',
+		title: '# Others',
 		link: ''
 	}
 });
-var Categorys = Backbone.Collection.extend({});
+var Categories = Backbone.Collection.extend({});
+var categories = new Categories();
 
 var CategoryView = Backbone.View.extend({
 	model: new Category(),
 	tagName: 'tr',
 	initialize: function() {
-		this.template = _.template($('.categorys-list-template').html());
+		this.template = _.template($('.category-titles-list-template').html());
+	},
+	events: {
+		'click .edit-category': 'edit',
+		'click .update-category': 'update'
+		
+	},
+	edit: function() {
+		$('.edit-category').hide();
+		$('.delete-category').hide();
+		this.$('.update-category').show();
+		this.$('.cancel-category').show();
+
+		var title = this.$('.category-title').html();
+		var link = this.$('.link').html();
+
+		this.$('.category-title').html('<input type="text" class="form-control category-title-update" value="' + title + '">');
+		this.$('.link').html('<input type="text" class="form-control link-update" value="' + link + '">');
+	},
+	update: function() {
+		this.model.set('category-title', $('.category-title-update').val());
+		this.model.set('link', $('.link-update').val());
+	},
+	cancel: function() {
+		categoriesView.render();
+	},
+	delete: function() {
+		this.model.destroy();
 	},
 	render: function() {
 		this.$el.html(this.template(this.model.toJSON()))
+		return this;
 	}
 });
 
-var CategorysView = Backbone.View.extend({
-	model: Categorys,
-	el: $('.categorys-list'),
+var CategoriesView = Backbone.View.extend({
+	model: categories,
+	el: $('.category-titles-list'),
 	initialize: function() {
-		this.model.on('add', this.render(), this);
+		var self = this;
+		this.model.on('add', this.render, this);
+		this.model.on('change', function() {
+			setTimeout(function() {
+				self.render();
+			}, 30);
+		}, this);
+		this.model.on('remove', this.render, this);
 	},
 	render: function() {
 		var self = this;
 		this.$el.html('');
-		_.each(this.model.toArray(), function(category) {
-			self.$el.append((new CategoryView({model: category})).render().$el);
+		_.each(this.model.toArray(), function(title) {
+			self.$el.append((new CategoryView({model: title})).render().$el);
 		});
+		return this;
 	}
 });
+var categoriesView = new CategoriesView();
+
+$(document).ready(function() {
+	$('.add-category').on('click', function() {
+		var category = new Category({
+			title: $('.category-title-input').val(),
+			link: $('.link-input').val()
+		});
+		$('.category-title-input').val('');
+		$('.link-input').val('');
+		console.log(category.toJSON());
+		categories.add(category);
+	})
+})
