04889b649f1f5221746578f118a691ae0189c62c
Kolawole ERINOSO <superprofessor23@gmail.com>
2015-05-20 09:44:00 +0100

    "Edits the index and js files"

---
+++ src/kola/linkvoting.js
@@ -0,0 +1,36 @@
+// Backbone Model
+
+var Category = Backbone.Model.extend({
+	defaults: {
+		hashtagCategory: 'Others',
+		title: '',
+		link: ''
+	}
+});
+var Categorys = Backbone.Collection.extend({});
+
+var CategoryView = Backbone.View.extend({
+	model: new Category(),
+	tagName: 'tr',
+	initialize: function() {
+		this.template = _.template($('.categorys-list-template').html());
+	},
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()))
+	}
+});
+
+var CategorysView = Backbone.View.extend({
+	model: categorys,
+	el: $('.categorys-list'),
+	initialize: function() {
+		this.model.on('add', this.render(), this);
+	},
+	render: function() {
+		var self = this;
+		this.$el.html('');
+		_.each(this.model.toArray(), function(category) {
+			self.$el.append((new CategoryView({model: category})).render().$el);
+		});
+	}
+});
